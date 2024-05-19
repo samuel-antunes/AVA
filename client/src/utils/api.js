921d52de7f942eb9c22d8dab4805c7dfa5a297ae
samuel-antunes <samuel_antunes1@hotmail.com>
2024-05-19 10:10:56 -0700

    debugging url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const API_URL = process.env.BACKEND_URL || "http://localhost:4000/api";
+console.log(API_URL);
 
 export const sendMessage = async (
   conversation,
