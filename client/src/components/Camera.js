2fecf40116863d1e6f5fd8063feea563b6e633e0
cas32 <claire_shou@sfu.ca>
2024-05-18 17:44:34 -0700

    added a camera for capture images to analyze

---
+++ client/src/components/Camera.js
@@ -0,0 +1,37 @@
+import { useCallback, useRef, useState } from "react";
+import React from "react";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+    width: 640,
+    height: 480,
+    facingMode: "user",
+};
+
+const Camera = () => {
+
+    const webcamRef = useRef("");
+    const [imgSrc, setImgSrc] = useState("");
+      
+        const capture = useCallback(() => {
+          const imageSrc = webcamRef.current.getScreenshot();
+          setImgSrc(imageSrc);
+        }, [webcamRef, setImgSrc]);
+      
+        return (
+          <>
+            <Webcam
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+              minScreenshotWidth={180}
+              minScreenshotHeight={180}
+            />
+            <button onClick={capture}>Capture Photo</button>
+            {imgSrc && <img src={imgSrc} alt="img" />}
+          </>
+        );
+};
+
+export default Camera;
