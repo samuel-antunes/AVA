3597831b54e03a9486a380293201de900b83c628
cas32 <claire_shou@sfu.ca>
2024-05-18 22:37:33 -0700

    working landing page, TO DO: add chatbot navigation page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,5 +1,7 @@
 import Chat from "../components/Chat";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import LogoComponent from "@/components/LogoComponenet";
+import GetStarted from "@/components/GetStarted";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "../utils/fontawesome";
 
@@ -12,7 +14,9 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 font-light" >
+      <LogoComponent />
+      <GetStarted />
       <h1 className="text-3xl font-bold mb-8">Chat with AVA</h1>
       <Chat />
     </div>
---
+++ client/src/components/GetStarted.js
@@ -0,0 +1,49 @@
+'use client'
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
+const GetStarted = () => {
+    const router = useRouter();
+    
+    const handleClick = () => {
+        router.push("/another-page");
+    };
+
+    return (
+        <div className="w-screen h-screen flex justify-center items-end pb-10">
+        <div className="relative bg-[#E5E5E5] rounded-3xl shadow-lg" style={{ width: '550px', height: '249px' }}>
+          {/* Yellow rectangle */}
+          <div 
+            className="relative bg-[#E7FF97] shadow-lg" 
+            style={{ width: '250px', height: '12px', marginTop: '45px', marginLeft: '150px'}}>
+          </div>
+  
+          {/* Text content */}
+          <div className="absolute inset-0 bottom-12 flex flex-col justify-center items-center">
+            <span 
+                className="font-black text-3xl mb-5" 
+                style={{ marginBottom: '10px' }}>
+                Introducing Ava
+            </span>
+            <span 
+                className="text-center px-20 py-2 mb-7"
+                style={{ marginLeft: '15px', marginRight: '15px' }}>
+                Your automated verbal assistant to bridge the gaps between languages</span>
+          </div>
+  
+          {/* Get Started button */}
+          <div className="absolute bottom-6 w-full flex justify-center">
+                <button 
+                    className="bg-[#E7FF97] px-12 py-2 rounded-3xl text-lg font-black" 
+                    style={{height: '50px' }}
+                    onClick={handleClick}>
+                    Get Started
+                </button>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default GetStarted;
